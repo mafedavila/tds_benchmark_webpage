dd69fb4ee43c41063dfb07143b33f8c9690493dd
Mafe Davila <mariafernandadavila3@gmail.com>
2026-01-05 13:18:13 -0500

    update for gh pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Suspense } from "react";
 
-//pages
 import Home from "./pages/Home/Home";
 import Publications from "./pages/Publications/Publications";
 import Synthesis from "./pages/Synthesis/Synthesis";
@@ -11,47 +10,19 @@ import GeneralUser from "./pages/Synthesis/pages/GeneralUser";
 import Researchers from "./pages/Synthesis/pages/Researchers";
 
 const AppRoutes = () => (
-    <BrowserRouter>
+    <HashRouter>
         <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-            <Route path="/" 
-                element={
-                    <Home />
-                } 
-            />
-            <Route path="/publications" 
-                element={
-                    <Publications />
-                } 
-            />
-            <Route path="/synthesis"
-                element={
-                    <Synthesis />
-                }
-            />
-            <Route path="/synthesis/general-users"
-                element={
-                    <GeneralUser />
-                }
-            />
-            <Route path="/synthesis/researchers"
-                element={
-                    <Researchers />
-                }
-            />
-            <Route path="/cv"
-                element={
-                    <Curriculum />
-                }
-            />
-            <Route path="/dissertation"
-                element={
-                    <Dissertation />
-                }
-            />
+            <Route path="/" element={<Home />} />
+            <Route path="/publications" element={<Publications />} />
+            <Route path="/synthesis" element={<Synthesis />} />
+            <Route path="/synthesis/general-users" element={<GeneralUser />} />
+            <Route path="/synthesis/researchers" element={<Researchers />} />
+            <Route path="/cv" element={<Curriculum />} />
+            <Route path="/dissertation" element={<Dissertation />} />
         </Routes>
         </Suspense>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 export default AppRoutes;
