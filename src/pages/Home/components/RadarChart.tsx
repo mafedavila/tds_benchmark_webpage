c763b9f8c65b8d2d200baa55dfe30eba53ad377f
mafedavila <mariafernandadavila3@gmail.com>
2026-04-10 19:29:46 +0200

    corrections to radar plots, new evaluation dimensions

--- src/pages/Home/components/RadarChart.tsx
+++ src/pages/Home/components/RadarChart.tsx
@@ -1,34 +1,43 @@
 import {
-    Radar,
-    RadarChart,
-    PolarGrid,
-    PolarAngleAxis,
-    PolarRadiusAxis,
-    ResponsiveContainer
+  Radar,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  ResponsiveContainer
 } from "recharts";
 
 interface RadarProps {
-    data: { label: string; value: number }[];
-    toolName: string;
+  data: { label: string; value: number }[];
+  toolName: string;
 }
 
 export default function RadarChartComponent({ data, toolName }: RadarProps) {
-    return (
-        <div className="w-full h-64">
-        <ResponsiveContainer>
-            <RadarChart data={data}>
-                <PolarGrid />
-                <PolarAngleAxis dataKey="label" />
-                <PolarRadiusAxis angle={30} domain={[0, 5]} />
-                <Radar
-                    name={toolName}
-                    dataKey="value"
-                    stroke="#2563eb"
-                    fill="#3b82f6"
-                    fillOpacity={0.6}
-                />
-            </RadarChart>
-        </ResponsiveContainer>
-        </div>
-    );
+  return (
+    <div className="w-full h-64">
+      <ResponsiveContainer>
+        <RadarChart data={data} startAngle={90} endAngle={-270}>
+          <PolarGrid gridType="circle" />
+
+          <PolarAngleAxis dataKey="label" />
+
+          <PolarRadiusAxis
+            angle={90}
+            domain={[11, 0]}
+            ticks={[0, 2, 4, 6, 8, 10] as any}
+            tickCount={6}
+            allowDataOverflow
+          />
+
+          <Radar
+            name={toolName}
+            dataKey="value"
+            stroke="#2563eb"
+            fill="#3b82f6"
+            fillOpacity={0.6}
+          />
+        </RadarChart>
+      </ResponsiveContainer>
+    </div>
+  );
 }
--- src/pages/Synthesis/pages/utils.ts
+++ src/pages/Synthesis/pages/utils.ts
@@ -9,157 +9,93 @@ export const getFiles = (toolName: string): string => {
 
 
 export const toolCharacteristics: Record<string, { label: string; value: number }[]> = {
-    "Sampling": [
-        { label: "Robustness", value: 8 },
-        { label: "Marginal Distributions", value: 8 },
-        { label: "Dependency Structure", value: 7 },
-        { label: "Comp. Performance", value: 8 },
-        { label: "Utility", value: 6 },
-        { label: "Privacy", value: 4 }
-    ],
-    "Bayesian Network": [
-        { label: "Robustness", value: 6 },
-        { label: "Marginal Distributions", value: 6 },
-        { label: "Dependency Structure", value: 7 },
-        { label: "Comp. Performance", value: 8 },
-        { label: "Utility", value: 5 },
-        { label: "Privacy", value: 6 }
-    ],
-    "CTAB-GAN": [
-        { label: "Robustness", value: 5 },
-        { label: "Marginal Distributions", value: 6 },
-        { label: "Dependency Structure", value: 7 },
-        { label: "Comp. Performance", value: 7 },
-        { label: "Utility", value: 8 },
-        { label: "Privacy", value: 7 }
+    "AutoDiff": [
+        { label: "Marginal Distribution", value: 3.83 },
+        { label: "Dependency Structure", value: 4.21 },
+        { label: "Robustness", value: 2.97 },
+        { label: "Privacy", value: 4.37 },
+        { label: "Utility", value: 3.61 },
+        { label: "Comp. Performance", value: 6.27 }
     ],
     "CTAB-GAN+": [
-        { label: "Robustness", value: 5 },
-        { label: "Marginal Distributions", value: 6 },
-        { label: "Dependency Structure", value: 7 },
-        { label: "Comp. Performance", value: 7 },
-        { label: "Utility", value: 8 },
-        { label: "Privacy", value: 7 }
+        { label: "Marginal Distribution", value: 6.90 },
+        { label: "Dependency Structure", value: 6.57 },
+        { label: "Robustness", value: 6.52 },
+        { label: "Privacy", value: 3.43 },
+        { label: "Utility", value: 6.73 },
+        { label: "Comp. Performance", value: 7.82 }
     ],
-    "VAE": [
-        { label: "Robustness", value: 5 },
-        { label: "Marginal Distributions", value: 6 },
-        { label: "Dependency Structure", value: 7 },
-        { label: "Comp. Performance", value: 7 },
-        { label: "Utility", value: 6 },
-        { label: "Privacy", value: 5 }
+    "CTGAN": [
+        { label: "Marginal Distribution", value: 7.43 },
+        { label: "Dependency Structure", value: 8.36 },
+        { label: "Robustness", value: 8.77 },
+        { label: "Privacy", value: 5.17 },
+        { label: "Utility", value: 9.05 },
+        { label: "Comp. Performance", value: 5.35 }
     ],
-    "TabDDPM": [
-        { label: "Robustness", value: 8 },
-        { label: "Marginal Distributions", value: 8 },
-        { label: "Dependency Structure", value: 7 },
-        { label: "Comp. Performance", value: 8 },
-        { label: "Utility", value: 7 },
-        { label: "Privacy", value: 9 }
-    ],
-    "GAP": [
-        { label: "Robustness", value: 3 },
-        { label: "Marginal Distributions", value: 3 },
-        { label: "Dependency Structure", value: 4 },
-        { label: "Comp. Performance", value: 4 },
-        { label: "Utility", value: 3 },
-        { label: "Privacy", value: 2 }
-    ],
-    "AutoDiff": [
-        { label: "Robustness", value: 8 },
-        { label: "Marginal Distributions", value: 9 },
-        { label: "Dependency Structure", value: 7 },
-        { label: "Comp. Performance", value: 8 },
-        { label: "Utility", value: 9 },
-        { label: "Privacy", value: 7 }
-    ],
-    "TabSyn": [
-        { label: "Robustness", value: 8 },
-        { label: "Marginal Distributions", value: 9 },
-        { label: "Dependency Structure", value: 7 },
-        { label: "Comp. Performance", value: 8 },
-        { label: "Utility", value: 9 },
-        { label: "Privacy", value: 7 }
-    ],
-    "REaLTabFormer": [
-        { label: "Robustness", value: 8 },
-        { label: "Marginal Distributions", value: 9 },
-        { label: "Dependency Structure", value: 8 },
-        { label: "Comp. Performance", value: 6 },
-        { label: "Utility", value: 9 },
-        { label: "Privacy", value: 7 }
+    "GANBLR++": [
+        { label: "Marginal Distribution", value: 10.37 },
+        { label: "Dependency Structure", value: 10.29 },
+        { label: "Robustness", value: 11 },
+        { label: "Privacy", value: 11 },
+        { label: "Utility", value: 10.54 },
+        { label: "Comp. Performance", value: 7.11 }
     ],
     "GReaT": [
-        { label: "Robustness", value: 8 },
-        { label: "Marginal Distributions", value: 9 },
-        { label: "Dependency Structure", value: 8 },
-        { label: "Comp. Performance", value: 6 },
-        { label: "Utility", value: 9 },
-        { label: "Privacy", value: 7 }
+        { label: "Marginal Distribution", value: 8.58 },
+        { label: "Dependency Structure", value: 9.18 },
+        { label: "Robustness", value: 6.67 },
+        { label: "Privacy", value: 4.72 },
+        { label: "Utility", value: 9.15 },
+        { label: "Comp. Performance", value: 7.12 }
     ],
-    "TabuLa": [
-        { label: "Robustness", value: 8 },
-        { label: "Marginal Distributions", value: 9 },
-        { label: "Dependency Structure", value: 8 },
-        { label: "Comp. Performance", value: 6 },
-        { label: "Utility", value: 9 },
-        { label: "Privacy", value: 7 }
+    "REaLTabFormer": [
+        { label: "Marginal Distribution", value: 3.60 },
+        { label: "Dependency Structure", value: 3.46 },
+        { label: "Robustness", value: 4.75 },
+        { label: "Privacy", value: 7.67 },
+        { label: "Utility", value: 3.67 },
+        { label: "Comp. Performance", value: 9.02 }
     ],
-    "C3TGAN": [
-        { label: "Robustness", value: 8 },
-        { label: "Marginal Distributions", value: 9 },
-        { label: "Dependency Structure", value: 8 },
-        { label: "Comp. Performance", value: 6 },
-        { label: "Utility", value: 9 },
-        { label: "Privacy", value: 7 }
+    "SMOTE": [
+        { label: "Marginal Distribution", value: 1.27 },
+        { label: "Dependency Structure", value: 4.39 },
+        { label: "Robustness", value: 1.20 },
+        { label: "Privacy", value: 5.70 },
+        { label: "Utility", value: 5.34 },
+        { label: "Comp. Performance", value: 5.53 }
     ],
-    "Kamino": [
-        { label: "Robustness", value: 8 },
-        { label: "Marginal Distributions", value: 9 },
-        { label: "Dependency Structure", value: 8 },
-        { label: "Comp. Performance", value: 6 },
-        { label: "Utility", value: 9 },
-        { label: "Privacy", value: 7 }
+    "TVAE": [
+        { label: "Marginal Distribution", value: 7.82 },
+        { label: "Dependency Structure", value: 9.75 },
+        { label: "Robustness", value: 7.23 },
+        { label: "Privacy", value: 3.70 },
+        { label: "Utility", value: 6.21 },
+        { label: "Comp. Performance", value: 5.27 }
     ],
-    "PrivLava": [
-        { label: "Robustness", value: 8 },
-        { label: "Marginal Distributions", value: 9 },
-        { label: "Dependency Structure", value: 8 },
-        { label: "Comp. Performance", value: 6 },
-        { label: "Utility", value: 9 },
-        { label: "Privacy", value: 7 }
+    "TabDDPM": [
+        { label: "Marginal Distribution", value: 6.15 },
+        { label: "Dependency Structure", value: 6.86 },
+        { label: "Robustness", value: 10 },
+        { label: "Privacy", value: 7.30 },
+        { label: "Utility", value: 5.52 },
+        { label: "Comp. Performance", value: 6.58 }
     ],
-    "TimeVAE": [
-        { label: "Robustness", value: 8 },
-        { label: "Marginal Distributions", value: 9 },
-        { label: "Dependency Structure", value: 8 },
-        { label: "Comp. Performance", value: 6 },
-        { label: "Utility", value: 9 },
-        { label: "Privacy", value: 7 }
+    "TabSyn": [
+        { label: "Marginal Distribution", value: 2.07 },
+        { label: "Dependency Structure", value: 6.79 },
+        { label: "Robustness", value: 2.50 },
+        { label: "Privacy", value: 7.47 },
+        { label: "Utility", value: 2.83 },
+        { label: "Comp. Performance", value: 6.42 }
     ],
-    "TimeGAN": [
-        { label: "Robustness", value: 8 },
-        { label: "Marginal Distributions", value: 9 },
-        { label: "Dependency Structure", value: 8 },
-        { label: "Comp. Performance", value: 6 },
-        { label: "Utility", value: 9 },
-        { label: "Privacy", value: 7 }
-    ],
-    "TSGM": [
-        { label: "Robustness", value: 8 },
-        { label: "Marginal Distributions", value: 9 },
-        { label: "Dependency Structure", value: 8 },
-        { label: "Comp. Performance", value: 6 },
-        { label: "Utility", value: 9 },
-        { label: "Privacy", value: 7 }
-    ],
-    "DoppelGANger": [
-        { label: "Robustness", value: 8 },
-        { label: "Marginal Distributions", value: 9 },
-        { label: "Dependency Structure", value: 8 },
-        { label: "Comp. Performance", value: 6 },
-        { label: "Utility", value: 9 },
-        { label: "Privacy", value: 7 }
+    "TabuLaMiddle": [
+        { label: "Marginal Distribution", value: 8.17 },
+        { label: "Dependency Structure", value: 6.46 },
+        { label: "Robustness", value: 4.42 },
+        { label: "Privacy", value: 5.73 },
+        { label: "Utility", value: 3.34 },
+        { label: "Comp. Performance", value: 10.92 }
     ]
 };
 
